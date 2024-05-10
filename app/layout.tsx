ef12250fe96d32d1c5522d760c936a33b519aecb
Joo Chanhwi <158996540+30kb@users.noreply.github.com>
2024-05-10 19:34:31 +0900

    feat: font

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,16 +1,10 @@
 import { type Metadata } from 'next';
-import localFont from 'next/font/local';
 
 import { type PropsWithChildren } from 'react';
 
 import QueryProvider from '@/providers/query-provider';
 
 import './globals.css';
-
-const wantedSansVariable = localFont({
-  src: '../public/fonts/WantedSansVariable.woff2',
-  display: 'swap',
-});
 
 const metadata: Metadata = {
   title: 'Surveey',
@@ -21,7 +15,7 @@ const RootLayout = ({ children }: PropsWithChildren) => {
   return (
     <html lang="ko-KR">
       <QueryProvider>
-        <body className={wantedSansVariable.className}>{children}</body>
+        <body>{children}</body>
       </QueryProvider>
     </html>
   );
--- app/page.tsx
+++ app/page.tsx
@@ -2,25 +2,7 @@ import { cn } from '@/lib/utils';
 
 const RootPage = () => {
   return (
-    <main
-      className={cn(
-        'h-screen w-screen',
-        'flex flex-col items-center justify-center',
-        'bg-neutral-50',
-      )}
-    >
-      <button
-        className={cn(
-          'flex flex-col items-center justify-center gap-10',
-          'hover:scale-110',
-          'active:scale-100',
-          'transition-transform',
-        )}
-      >
-        <p className="text-8xl">👋</p>
-        <p className="text-4xl font-bold">Hello World</p>
-      </button>
-    </main>
+    <main className="flex h-screen w-screen flex-col items-center justify-center bg-neutral-50"></main>
   );
 };
 
