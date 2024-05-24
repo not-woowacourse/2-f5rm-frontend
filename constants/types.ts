a9693167ebd73007b85415e3817391a0b0fe3896
smreosms13 <smreosms13@gamil.com>
2024-05-24 17:11:30 +0900

    feat: define types for input, metadata, restriction

---
+++ constants/types.ts
@@ -0,0 +1,59 @@
+import type { ComponentPropsWithoutRef } from 'react';
+
+import type { Schema } from 'zod';
+
+import type { Input } from '@/components/ui/input';
+
+export type ItemValue = {
+  value: string;
+};
+
+type DefaultRestriction = {
+  restrictions: Schema;
+};
+
+export enum InputType {
+  Text = 'text',
+  URL = 'url',
+  Email = 'email',
+  Number = 'number',
+  Tel = 'tel',
+}
+
+type TextInputAnswer = DefaultRestriction & {
+  type: InputType;
+  description?: string;
+  prefix?: string;
+  suffix?: string;
+} & Pick<ComponentPropsWithoutRef<typeof Input>, 'placeholder'>;
+
+type SelectAnswer = DefaultRestriction & {
+  type: 'select';
+  items: ItemValue[];
+};
+
+export type MultiSelectOption = {
+  id: string; // should be unique
+  title: string;
+  description?: string;
+};
+
+type MultiSelectAnswer = DefaultRestriction & {
+  type: 'multiselect';
+  options: MultiSelectOption[];
+};
+
+export type Answer = TextInputAnswer | SelectAnswer | MultiSelectAnswer;
+
+interface Item {
+  id: string; // should be unique
+  question: string;
+  description: string;
+  answer: Answer;
+}
+
+export interface Metadata {
+  title: string;
+  description: string;
+  items: Item[];
+}
