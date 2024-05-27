3b241fccece07361625927fdde5fabb85a6455f7
smreosms13 <smreosms13@gamil.com>
2024-05-27 20:02:33 +0900

    feat: add function to determine age group of answer

--- lib/utils.ts
+++ lib/utils.ts
@@ -4,3 +4,22 @@ import { twMerge } from 'tailwind-merge';
 export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs));
 }
+
+export function determineAgeGroup(age: number): string {
+  switch (Math.floor(age / 10)) {
+    case 1:
+      return '10대';
+    case 2:
+      return '20대';
+    case 3:
+      return '30대';
+    case 4:
+      return '40대';
+    case 5:
+      return '50대';
+    case 6:
+      return '60대';
+    default:
+      return age < 10 ? '10대 미만' : '70대 이상';
+  }
+}
